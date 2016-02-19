414c8e5c50011bbf56142d8a2ded20b0e53855e5
madgodofmagic <mad@janitor.chat>
2016-02-19 00:14:44 +0000

    wait on all threads to finish, parse server lines in parallel

--- MultiSSH.hs
+++ MultiSSH.hs
@@ -1,17 +1,17 @@
 -- build with -threaded or forkIO does nothing
 -- TODO: Allow server list to be entered/piped in via stdin
 -- TODO: Optionally allow custom remote username for a server
--- TODO: Wait on all threads instead of blocking in a 'getch()'
 -- TODO: Allow different key algorithms and/or custom key locations
--- BUG: Probably upstream - segfault when cwd not included in relative path
--- BUG: Could be some implicit timeout not being hit, I couldn't see any errors reported for stuff with incorrect ssh port specified
+-- BUG: Probably upstream - segfault when cwd not included in relative path UPDATE: Yeah this is libssh2 being difficult
+-- CAVEAT: There doesn't seem to be a good way to force a timeout or even cancel a connection if it's waiting for the handshake to complete, the timeout here only cancels long command execution, the ssh timeout will always be based on system-wide defaults (TODO: Find a less minimalistic ssh lib)
+-- Until then a. interrupt the process b. set a tcp timeout system-wide or c. don't put dead servers (i.e. the destinations which won't respond with an RST) in the config
 -- TODO: allow passphrase to be entered before opening key
 import Network.SSH.Client.SimpleSSH
-import Control.Concurrent
+import Control.Monad.Parallel as ParM
 import System.Environment
 import System.Posix.User
 import Text.Read
-
+import System.Timeout
 import qualified Data.Text as T
 data SSHEnv = SSHEnv String String String String
 data ServerAddress = ServerAddress String Integer
@@ -27,32 +27,50 @@ buildenv key_algorithm = do
       privkey = ssh_dir ++ "/id_" ++ key_algorithm
   return $ SSHEnv known_hosts pubkey privkey passphrase
 
-runCommandOnHost :: String -> Integer -> String -> String -> IO ()
-runCommandOnHost host port username command = do
+
+
+
+
+runCommandOnHost
+  :: [Char] -> Integer -> String -> String -> Int -> IO ()
+runCommandOnHost host port username command timeout_secs = do
   -- assume rsa
   (SSHEnv known_hosts pubkey privkey passphrase) <- buildenv "rsa"
-  let client = withSessionKey host port known_hosts username pubkey privkey passphrase
-  res <- runSimpleSSH $ client $ buildCommandAction command
+  
+  let client = withSessionKey host port known_hosts username pubkey privkey passphrase 
+  r <- timeout (timeout_secs * 1000000) $ runSimpleSSH $ client $ buildCommandAction command
+  let res = case r of
+        Nothing -> Left(Connect)
+        Just(goodres) -> goodres
   case res of
-    Left err -> putStrLn $ show err ++ " error raised on " ++ host
-    Right (Result stdOut stdErr exitCode) ->
-      let prettify = read . show in
-      putStr $ foldl (++) "Host: " [host,"\n","Out: ",prettify stdOut,"Err: ",prettify stdErr,"Exit: ",show exitCode,"\n\n"]
+    Left err -> do let message = show err ++ " error raised on " ++ host ++ "\n"
+                   putStr message
+
+    Right (Result stdOut stdErr exitCode) -> do
+      putStrLn $ "Working on " ++ host
+      let prettify = read . show
+          message = foldl (++) "Host: " [host,"\n","Out: ",prettify stdOut,"Err: ",prettify stdErr,"Exit: ",show exitCode,"\n\n"]
+      putStrLn message
+
 
 buildCommandAction :: String -> Session -> SimpleSSH Result
 buildCommandAction command session = execCommand session command
 
-dispatch_threads :: String -> String -> FilePath -> IO ()
-dispatch_threads username command serverlist = do
-    hosts <- parse_serverlist serverlist
-    let makeThread (Just (ServerAddress server port)) = do _ <- ($) forkIO $ runCommandOnHost server port username command
-                                                           return ()
-        makeThread Nothing = do return ()
-      in
-      mapM_ makeThread hosts
 
-block :: IO ()
-block = interact $ take 1
+
+
+
+
+
+
+dispatch_threads :: String -> String -> FilePath -> Int -> IO ()
+dispatch_threads username command serverlist timeout_secs = do
+    hosts <- parse_serverlist serverlist
+    let makeThread (Just (ServerAddress server port)) = runCommandOnHost server port username command timeout_secs >> return ()
+                                                           
+        makeThread Nothing = return ()
+      in
+      ParM.mapM_ makeThread hosts
 
 demangle_server :: [Char] -> IO (Maybe ServerAddress)
 demangle_server serverstring =
@@ -70,15 +88,16 @@ demangle_server serverstring =
 parse_serverlist :: FilePath -> IO [Maybe ServerAddress]
 parse_serverlist listpath = do
  thelist <- readFile listpath
- mapM demangle_server $ lines thelist
+ ParM.mapM demangle_server $ lines thelist
 
 main :: IO ()
 main = do
   args <- getArgs
   myname <- getProgName
   case args of
-    (username:command:serverlist:[]) -> do
+    (username:command:serverlist:timeout_secs:[]) -> do
       putStrLn $ "Running " ++ command ++ " as " ++ username
-      dispatch_threads username command serverlist
-      block
-    _ -> fail $ "Usage: " ++ myname ++ " remote_username command serverlist_filename"
+      dispatch_threads username command serverlist $ read timeout_secs
+      putStrLn "Done."
+    _ -> fail $ "Usage: " ++ myname ++ " remote_username command serverlist_filename timeout_secs"
+    
